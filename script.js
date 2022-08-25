a69c1a5ac24f140085a7981b620d4b023e28bd06
MO5E2 <76234330+MO5E2@users.noreply.github.com>
2022-08-24 22:17:14 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function show_main() {
+    var intro = document.getElementById("intro");
+    var main = document.getElementById("main")
+    intro.style.visibility = "hidden"
+    main.style.visibility = "visible"
+}
+function show_intro() {
+    var intro = document.getElementById("intro");
+    var main = document.getElementById("main")
+    intro.style.visibility = "visible"
+    main.style.visibility = "hidden"
+}
